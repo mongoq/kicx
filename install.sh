9e3b634b65611618cdfecdad35cd5d4ef4db452a
mongoq <mongoq@das-labor.org>
2026-07-07 19:43:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Welcome to kicx! We are preparing to talk to a Tang Nano 9k ..."
+echo "Welcome to kicx!"
+echo "Setting up your FPGA development environment..."
 
 echo "Installing required packages..."
 
-sudo apt update
-sudo apt install -y \
+sudo apt-get update
+sudo apt-get install -y \
     git \
     curl \
     unzip \
@@ -25,12 +26,14 @@ fi
 
 cd "$INSTALL_DIR"
 
+echo
 echo "Starting virtual machine..."
 vagrant up
 
-echo
-echo "Done!"
-echo
-echo "To connect:"
+echo "To connect later:"
 echo "cd $INSTALL_DIR"
 echo "vagrant ssh"
+
+echo
+echo "Connecting to the virtual machine..."
+exec vagrant ssh
